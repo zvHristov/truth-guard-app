9fe961ba5c46b1636c8b6243ca6366f6da7be9c9
zvHristov <zizotruerider@gmail.com>
2024-12-16 14:09:12 +0200

    build: added prettier setup, tailwindcss plugin, sort imports plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,30 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+import { FlatCompat } from "@eslint/eslintrc"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript", "plugin:tailwindcss/recommended"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "plugin:tailwindcss/recommended"
+  ),
   ...compat.rules({
-    "no-console": ["error", { "allow": ["info", "warn", "error"] }],
+    "no-console": ["error", { allow: ["info", "warn", "error"] }],
     "tailwindcss/classnames-order": "error",
   }),
   ...compat.overrides([
     {
-      "files": ["*.ts", "*.tsx", "*.js"],
-      "parser": "@typescript-eslint/parser"
-    }
+      files: ["*.ts", "*.tsx", "*.js"],
+      parser: "@typescript-eslint/parser",
+    },
   ]),
-];
+]
 
-export default eslintConfig;
+export default eslintConfig
