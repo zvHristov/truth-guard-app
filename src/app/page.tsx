050a7679805730c7518ec343c0edcf01435da7c9
zvHristov <zizotruerider@gmail.com>
2024-12-17 18:12:59 +0200

    fix: remove params

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import dynamic from 'next/dynamic';
 const HomeComponent = dynamic(() => import('../components/components-pages/home-page/HomePage'), { ssr: false });
 
 
-export default function Home({ searchParams }: { searchParams: { [key: string]: string | string[] | undefined } }) {
+export default function Home() {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
       <h1>Article Truth Analyzer</h1>
