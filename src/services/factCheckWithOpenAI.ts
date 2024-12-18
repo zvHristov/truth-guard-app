1e1e3ad7869ce8e7bfd28033b9e6df7d88d16017
zvHristov <zizotruerider@gmail.com>
2024-12-18 10:35:20 +0200

    chore: better asking for AI

--- src/services/factCheckWithOpenAI.ts
+++ src/services/factCheckWithOpenAI.ts
@@ -16,7 +16,7 @@ export const factCheckWithOpenAI = async (text: string) => {
     const response: any = await openai.chat.completions.create({
       model: 'gpt-4o-mini',
       messages: [
-        { role: 'system', content: FACT_CHECK_EN_SYSTEM_AI_PROMPT },
+        { role: 'system', content: `${FACT_CHECK_EN_SYSTEM_AI_PROMPT}` },
         { role: 'user', content: `${FACT_CHECK_EN_USER_AI_PROMPT} ${text}` },
       ],
     });
@@ -30,26 +30,26 @@ export const factCheckWithOpenAI = async (text: string) => {
 
 export const propagandaCheckerOpenAI = async (content: string) => {
     try {
+        const prompt = `${PROPAGANDA_CHECKER_EN_CONTEXT_AI_TEXT_PROMPT} ${content} 
+            ${PROPAGANDA_CHECKER_EN_TELL_ME_AI_TEXT_PROMPT}
+        `;
 
-        const prompt = `${PROPAGANDA_CHECKER_EN_CONTEXT_AI_TEXT_PROMPT} ${content} ${PROPAGANDA_CHECKER_EN_TELL_ME_AI_TEXT_PROMPT}`;
-
-      const completion = await openai.chat.completions.create({
-        model: 'gpt-4o-mini',
-        messages: [
-            {
-              role: "system",
-              content: PROPAGANDA_CHECKER_EN_SYSTEM_AI_PROMPT
-            },
-            {
-              role: "user",
-              content: prompt
-            }
-          ],
-          temperature: 0.5,
-          max_tokens: 1000,
-          presence_penalty: 0.6,
-          frequency_penalty: 0.6,
- 
+        const completion = await openai.chat.completions.create({
+            model: 'gpt-4o-mini',
+            messages: [
+                {
+                role: "system",
+                content: PROPAGANDA_CHECKER_EN_SYSTEM_AI_PROMPT
+                },
+                {
+                role: "user",
+                content: prompt
+                }
+            ],
+            temperature: 0.5,
+            max_tokens: 1000,
+            presence_penalty: 0.6,
+            frequency_penalty: 0.6,
       });
   
       const result: any = completion.choices[0].message.content;
